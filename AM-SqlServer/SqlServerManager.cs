f15daed8a7bfba5592b5be33cd0eb5ce0618fea1
Andrew <holzmaad@mail.uc.edu>
2018-10-16 22:51:49 -0400

    local sql connection + pulling all agent jobs

--- AM-SqlServer/SqlServerManager.cs
+++ AM-SqlServer/SqlServerManager.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.SqlServer.Management.Common;
 using System;
+using System.Collections.Generic;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Smo.Agent;
 
 
 namespace AM_SqlServer
@@ -9,17 +12,86 @@ namespace AM_SqlServer
     /// </summary>
     public class SqlServerManager
     {
-        public bool ConnectToRemoteDatabase(string server, string instance, string username, string password)
+        public Server ConnectToRemoteDatabase(string server, string username, string password)
         {
             ServerConnection srvConn = new ServerConnection(server);
-            
-
-            return true;
+            srvConn.LoginSecure = false;
+            srvConn.Login = username;
+            srvConn.Password = password;
+            Server srv = new Server(srvConn);
+            srv.ConnectionContext.Connect();
+            if (srv.ConnectionContext.IsOpen)
+            {
+                return srv;
+            }
+            else
+            {
+                throw new Exception("Failed to connect to local SQL server");
+            }
         }
 
-        public bool ConnectToLocalDatabase(string instance, string username, string password)
+       
+
+        public Server ConnectToLocalDatabase(string username, string password)
         {
-            throw new NotImplementedException();
+            Server srv = new Server();
+            srv.ConnectionContext.LoginSecure = false; //set true for windows auth?
+            srv.ConnectionContext.Login = username;
+            srv.ConnectionContext.Password = password;
+            srv.ConnectionContext.Connect();
+            if (srv.ConnectionContext.IsOpen)
+            {
+                return srv;
+            }
+            else
+            {
+                throw new Exception("Failed to connect to local SQL server");
+            }
+        }
+
+        public bool DisconnectFromRemoteDatabase(Server srv)
+        {
+            if (srv.ConnectionContext.IsOpen)
+            {
+                srv.ConnectionContext.Disconnect();
+            }
+
+            if (srv.ConnectionContext.IsOpen)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public List<Job> GetAllSqlAgentJobs(string username, string password, string server = "localhost")
+        {
+            Server srv = null;
+            if (server == "localhost")
+            {
+                srv = ConnectToLocalDatabase(username, password);
+            }
+            else
+            {
+                srv = ConnectToRemoteDatabase(server, username, password);
+            }
+
+            if (srv != null)
+            {
+                List<Job> jobs = new List<Job>();
+                foreach (Job job in srv.JobServer.Jobs)
+                {
+                    jobs.Add(job);
+                }
+
+                return jobs;
+            }
+            else
+            {
+                throw new Exception("Srv is null make me a good exception do something else");
+            }
         }
     }
 }
--- AutomationManagerTests/SqlServerIntegrationTests.cs
+++ AutomationManagerTests/SqlServerIntegrationTests.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using AM_SqlServer;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Smo.Agent;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AutomationManagerTests
@@ -5,9 +9,19 @@ namespace AutomationManagerTests
     [TestClass]
     public class SqlServerIntegrationTests
     {
+        SqlServerManager _ssm = new SqlServerManager();
         [TestMethod]
-        public void TestMethod1()
+        public void LocalSqlServerConnectionSqlAuthShouldSucceed()
         {
+            Server srv = _ssm.ConnectToLocalDatabase("sa", "password");
+            Assert.IsNotNull(srv);
+        }
+
+        [TestMethod]
+        public void LocalSqlServerConnectionShouldReturnJobs()
+        {
+            List<Job> jobs = _ssm.GetAllSqlAgentJobs("sa", "password");
+            Assert.IsTrue(jobs.Count>0);
         }
     }
 }
