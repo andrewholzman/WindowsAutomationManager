83409ae229302f771c7142ac77b36522132bc866
Andrew (Dev Env) <holzmaad@mail.uc.edu>
2018-11-26 14:31:12 -0500

    full crud logic. Needs testing, especially Update.

--- AM-SqlServer/SqlServerManager.cs
+++ AM-SqlServer/SqlServerManager.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.SqlServer.Management.Smo;
 using Microsoft.SqlServer.Management.Smo.Agent;
-
+using System.Reflection;
 
 namespace AM_SqlServer
 {
@@ -12,6 +12,35 @@ namespace AM_SqlServer
     /// </summary>
     public class SqlServerManager
     {
+        private static string _server;
+        private static string _username;
+        private static string _password;
+
+        public SqlServerManager(string server, string username, string password)
+        {
+            _server = server;
+            _username = username;
+            _password = password;
+        }
+        public SqlServerManager() { } //empty constructor to allow manual creation for now
+
+        public Server ConnectToRemoteDatabase()
+        {
+            ServerConnection srvConn = new ServerConnection(_server);
+            srvConn.LoginSecure = false;
+            srvConn.Login = _username;
+            srvConn.Password = _password;
+            Server srv = new Server(srvConn);
+            srv.ConnectionContext.Connect();
+            if (srv.ConnectionContext.IsOpen)
+            {
+                return srv;
+            }
+            else
+            {
+                throw new Exception("Failed To Connect to SQL Job Server");
+            }
+        }
         public Server ConnectToRemoteDatabase(string server, string username, string password)
         {
             ServerConnection srvConn = new ServerConnection(server);
@@ -26,12 +55,26 @@ namespace AM_SqlServer
             }
             else
             {
-                throw new Exception("Failed to connect to local SQL server");
+                throw new Exception("Failed To Connect to SQL Job Server");
             }
         }
 
-       
-
+        public Server ConnectToLocalDatabase()
+        {
+            Server srv = new Server();
+            srv.ConnectionContext.LoginSecure = false; //set true for windows auth?
+            srv.ConnectionContext.Login = _username;
+            srv.ConnectionContext.Password = _password;
+            srv.ConnectionContext.Connect();
+            if (srv.ConnectionContext.IsOpen)
+            {
+                return srv;
+            }
+            else
+            {
+                throw new Exception("Failed To Connect to SQL Job Server");
+            }
+        }
         public Server ConnectToLocalDatabase(string username, string password)
         {
             Server srv = new Server();
@@ -45,7 +88,7 @@ namespace AM_SqlServer
             }
             else
             {
-                throw new Exception("Failed to connect to local SQL server");
+                throw new Exception("Failed To Connect to SQL Job Server");
             }
         }
 
@@ -66,6 +109,33 @@ namespace AM_SqlServer
             }
         }
 
+        public List<Job> GetAllSqlAgentJobs(string server = "localhost")
+        {
+            Server srv = null;
+            if (server == "localhost")
+            {
+                srv = ConnectToLocalDatabase(_username, _password);
+            }
+            else
+            {
+                srv = ConnectToRemoteDatabase(server, _username, _password);
+            }
+
+            if (srv != null)
+            {
+                List<Job> jobs = new List<Job>();
+                foreach (Job job in srv.JobServer.Jobs)
+                {
+                    jobs.Add(job);
+                }
+
+                return jobs;
+            }
+            else
+            {
+                throw new Exception("Failed To Connect to SQL Job Server");
+            }
+        }
         public List<Job> GetAllSqlAgentJobs(string username, string password, string server = "localhost")
         {
             Server srv = null;
@@ -90,19 +160,34 @@ namespace AM_SqlServer
             }
             else
             {
-                throw new Exception("Srv is null make me a good exception do something else");
+                throw new Exception("Failed To Connect to SQL Job Server");
             }
         }
 
-
-        public void CreateJob(Job job)
+        public Job FindJobByID(Guid jobId, string server = "localhost")
         {
-            throw new NotImplementedException();
-        }
+            Server srv = null;
+            if (server == "localhost")
+            {
+                srv = ConnectToLocalDatabase(_username, _password);
+            }
+            else
+            {
+                srv = ConnectToRemoteDatabase(server, _username, _password);
+            }
 
+            if (srv != null)
+            {
+                Job j = srv.JobServer.GetJobByID(jobId);
+                return j;
+            }
+            else
+            {
+                throw new Exception("Failed To Connect to SQL Job Server");
+            }
+        }
         public Job FindJobByID(Guid jobId, string username, string password, string server = "localhost")
         {
-        
             Server srv = null;
             if (server == "localhost")
             {
@@ -120,7 +205,200 @@ namespace AM_SqlServer
             }
             else
             {
-                throw new Exception("bad server");
+                throw new Exception("Failed To Connect to SQL Job Server");
+            }
+        }
+
+        public void CreateJob(Job job, string server = "localhost")
+        {
+            Server srv = null;
+            if (server == "localhost")
+            {
+                srv = ConnectToLocalDatabase(_username, _password);
+            }
+            else
+            {
+                srv = ConnectToRemoteDatabase(server, _username, _password);
+            }
+
+            if (srv != null)
+            {
+                try
+                {
+                    srv.JobServer.Jobs.Add(job);
+                } catch (Exception ex)
+                {
+                    throw new Exception("Failed To Create Job: " + job.Name + " - " + ex.Message);
+                }
+                
+            } else
+            {
+                throw new Exception("Failed To Connect to SQL Job Server");
+            }
+        }
+        public void CreateJob(Job job, string username, string password, string server = "localhost")
+        {
+            Server srv = null;
+            if (server == "localhost")
+            {
+                srv = ConnectToLocalDatabase(username, password);
+            }
+            else
+            {
+                srv = ConnectToRemoteDatabase(server, username, password);
+            }
+
+            if (srv != null)
+            {
+                try
+                {
+                    srv.JobServer.Jobs.Add(job);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Failed To Create Job: " + job.Name + " - " + ex.Message);
+                }
+
+            }
+            else
+            {
+                throw new Exception("Failed To Connect to SQL Job Server");
+            }
+        }
+
+        public void DeleteJob(Guid jobId, string server = "localhost")
+        {
+            Server srv = null;
+            if (server == "localhost")
+            {
+                srv = ConnectToLocalDatabase(_username, _password);
+            }
+            else
+            {
+                srv = ConnectToRemoteDatabase(server, _username, _password);
+            }
+
+            if (srv != null)
+            {
+                try
+                {
+                    srv.JobServer.DropJobByID(jobId);
+                } catch (Exception ex)
+                {
+                    throw new Exception("Failed To Drop Job: " + jobId.ToString() +" - " + ex.Message);
+                }
+                
+            } else
+            {
+                throw new Exception("Failed To Connect to SQL Job Server");
+            }
+        }
+        public void DeleteJob(Guid jobId, string username, string password, string server = "localhost")
+        {
+             Server srv = null;
+            if (server == "localhost")
+            {
+                srv = ConnectToLocalDatabase(username, password);
+            }
+            else
+            {
+                srv = ConnectToRemoteDatabase(server, username, password);
+            }
+
+            if (srv != null)
+            {
+                try
+                {
+                    srv.JobServer.DropJobByID(jobId);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Failed To Drop Job: " + jobId.ToString() + " - " + ex.Message);
+                }
+
+            }
+            else
+            {
+                throw new Exception("Failed To Connect to SQL Job Server");
+            }
+        }
+
+        public Job UpdateJobById(Job dirtyJob, string server = "localhost")
+        {
+            Server srv = null;
+            if (server == "localhost")
+            {
+                srv = ConnectToLocalDatabase(_username, _password);
+            }
+            else
+            {
+                srv = ConnectToRemoteDatabase(server, _username, _password);
+            }
+
+            if (srv != null)
+            {
+                try
+                {
+                    Job cleanJob = srv.JobServer.GetJobByID(dirtyJob.JobID);
+                    if (cleanJob != dirtyJob)
+                    {
+                        UpdateJob(cleanJob, dirtyJob);
+                    }
+                    return cleanJob;
+                } catch (Exception ex)
+                {
+                    throw new Exception("Failed To Update Job - " + ex.Message);
+                }
+                
+            } else
+            {
+                throw new Exception("Failed To Connect to SQL Job Server");
+            }
+        }
+        public Job UpdateJobById(Job dirtyJob, string username, string password, string server = "localhost")
+        {
+            Server srv = null;
+            if (server == "localhost")
+            {
+                srv = ConnectToLocalDatabase(username, password);
+            }
+            else
+            {
+                srv = ConnectToRemoteDatabase(server, username, password);
+            }
+
+            if (srv != null)
+            {
+                try
+                {
+                    Job cleanJob = srv.JobServer.GetJobByID(dirtyJob.JobID);
+                    if (cleanJob != dirtyJob)
+                    {
+                        UpdateJob(cleanJob, dirtyJob);
+                    }
+                    return cleanJob;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Failed To Update Job - " + ex.Message);
+                }
+
+            }
+            else
+            {
+                throw new Exception("Failed To Connect to SQL Job Server");
+            }
+        }
+
+        private void UpdateJob(Job cleanJob, Job dirtyJob)
+        {
+            foreach (PropertyInfo prop in cleanJob.GetType().GetProperties())
+            {
+                PropertyInfo dirtyProp = dirtyJob.GetType().GetProperty(prop.Name);
+                if (dirtyProp != prop)
+                {
+                    cleanJob.GetType().GetProperty(prop.Name).SetValue(cleanJob.GetType().GetProperty(prop.Name), dirtyProp);
+                }
             }
         }
     }
